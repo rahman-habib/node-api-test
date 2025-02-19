b7c8e5d776b2f65304d9929f7c51d71f08c75514
rahman-habib <habib.rahman4665@gmail.com>
2025-02-19 12:28:51 +0500

    commit

--- app.js
+++ app.js
@@ -1,18 +1,31 @@
-const bodyParser=require("body-parser");
-const cors=require("cors");
-const express=require("express");
-const port=process.env.PORT||9010
-const app=express();
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const express = require("express");
+const axios = require("axios");
 
-app.use(bodyParser.json(),cors())
+const port = process.env.PORT || 9010;
+const app = express();
 
+// Middleware
+app.use(bodyParser.json());
+app.use(cors());
 
-app.get("/",(req,res)=>{
-    res.send("Welcome to NodeJs API Project")
-})
+app.get("/", (req, res) => {
+    res.send("Welcome to NodeJs API Project");
+});
 
-app.get('/hello',(req,res)=>{
-    res.send("Hello World! i am here")
-})
+app.get("/hello", (req, res) => {
+    res.send("Hello World! I am here");
+});
 
-app.listen(port,()=>console.log(`server is start and running on ${port}`))
+app.get("/get", async (req, res) => {
+    try {
+        let response = await axios.get("https://baconipsum.com/api/?type=meat-and-filler");
+        res.json(response.data); // Send API response to the client
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+});
+
+app.listen(port, () => console.log(`Server is started and running on port ${port}`));
